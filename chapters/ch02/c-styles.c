192e9bf7054f36e6bcd6817cedd036f8570f6c85
takymt <takuto.yamamoto.dev@gmail.com>
2026-03-24 01:19:04 +0900

    chapter2: how-to-write-program

---
+++ chapters/ch02/c-styles.c
@@ -0,0 +1,16 @@
+int bad_style(void) { return 0; }
+
+int k_and_r(void) {
+    return 0;
+}
+
+int allman(void)
+{
+    return 0;
+}
+
+void main(void) {
+    bad_style();
+    k_and_r();
+    allman();
+}
---
+++ chapters/ch02/do_nothing_styled.c
@@ -0,0 +1,5 @@
+/* K & R style*/
+int main(void) {
+    /* tab size: 4 spaces*/
+    return 0;
+}
